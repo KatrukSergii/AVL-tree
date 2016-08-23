cd956b033cfee57b0e0adf1e7acd2941c73cb94b
sergii.katruk <katruksergyi@gmail.com>
2016-08-23 12:23:32 +0300

    Project structur. First classes.

---
+++ AVLTree/AVLTree/Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AVLTree
+{
+    public class Command : ICommand
+    {
+        private readonly Func<object, bool> canExec;
+
+        private readonly Action execAction;
+
+        private readonly Action<object> execParamAction;
+        public bool CanExecute(object parameter)
+        {
+            return this.canExec?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execAction?.Invoke();
+            this.execParamAction?.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        private Command(Func<object, bool> canExecFunc)
+        {
+            this.canExec = canExecFunc;
+        }
+
+        public Command(Action execAction, Func<object, bool> canExecFunc = null)
+            : this(canExecFunc)
+        {
+            this.execAction = execAction;
+        }
+
+        public Command(Action<object> execParamAction, Func<object, bool> canExecFunc)
+            : this(canExecFunc)
+        {
+            this.execParamAction = execParamAction;
+        }
+    }
+}
---
+++ AVLTree/AVLTree/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AVLTree
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand StartCommand => new Command(this.Start);
+
+        private void Start()
+        {
+        }
+    }
+}
