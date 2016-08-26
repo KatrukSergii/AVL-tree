b576f244bd3a137bc25197d00b98fb7d26251e55
sergii.katruk <sergii.katruk@WS78>
2016-08-26 17:50:15 +0300

    Node and tree were added.

---
+++ AVLTree/AVLTree/AvlTree/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AVLTree.AvlTree
+{
+    public class Node<T> where T : IComparable
+    {
+        public T Value { get; set; }
+        public Tree<T> Tree { get;}
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public T Find(Func<T, short> predicate)
+        {
+            var comparisionResult = predicate(this.Value);
+            if (comparisionResult == 0)
+                return this.Value;
+            if (comparisionResult > 0 && this.Right != null)
+                return this.Right.Find(predicate);
+            if (comparisionResult < 0 && this.Left != null)
+                return this.Left.Find(predicate);
+            return this.Tree.DefaultValue;
+        }
+        public Node(T value, Tree<T> tree = null)
+        {
+            this.Tree = tree;
+            this.Value = value;
+        }
+
+        public void Add(T item)
+        {
+        }
+
+        public void Romove(T item)
+        {
+            
+        }
+    }
+}
---
+++ AVLTree/AVLTree/AvlTree/Tree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree.AvlTree
+{
+    public class Tree<T> where T : IComparable
+    {
+        public T DefaultValue { get; }
+        public Node<T> Root { get; private set; }
+
+        public void Add(T item)
+        {
+            if (this.Root == null)
+            {
+                this.Root = new Node<T>(item, this);
+                return;
+            }
+            this.Root.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            if (this.Root == null)
+                return;
+            this.Root.Romove(item);
+        }
+        public T Find(Func<T, short> predicate)
+        {
+            if (this.Root == null)
+                return this.DefaultValue;
+            return this.Root.Find(predicate);
+        }
+        public Tree(T defaultValue = default(T))
+        {
+            
+        }
+    }
+}
